1eeb73bd503d73d4319bb096bb3a715dfc126b16
pyatnovov <109892815+pyatnovov@users.noreply.github.com>
2023-08-31 08:07:10 +0000

    Deploying to gh-pages from @ pyatnovov/ur_liceum_project@9262bd38ebaf7af805f282c9205966d2ae09c111 🚀

--- index.23a68766.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0});
-//# sourceMappingURL=index.23a68766.js.map
--- index.a088a05f.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0});
-//# sourceMappingURL=index.a088a05f.js.map
